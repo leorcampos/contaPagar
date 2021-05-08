ed5880852477b88f97ca1165fa80cce5d32d46dc
leorcampos <leonardor.campos1@gmail.com>
2021-05-08 12:06:16 -0300

    start Projeto

---
+++ contaPagar/src/main/java/testePratico/ContaPagar/ContaPagarApplication.java
@@ -0,0 +1,13 @@
+package testePratico.ContaPagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaPagarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaPagarApplication.class, args);
+	}
+
+}
